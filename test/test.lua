927f209a883d9d3cd823346a6f1d1f29a805e27f
Jonathan Tompson <tompson@cims.nyu.edu>
2014-07-22 12:13:23 -0400

    removed redundant kernels from DistKLDivCriterion and modified test.

--- test/test.lua
+++ test/test.lua
@@ -1853,26 +1853,9 @@ function cunntest.distkldiv()
       cutorch.synchronize()
       tm.gpu = a:time().real
 
-      local tm2 = {}
-      local title = string.format('DistKLDivCriterion2 sizeAverage %d, %d ',sizeAverage, size)
-      times[title] = tm2
-      tm2.cpu = tm.cpu
-      local cinput2 = input:cuda()
-      local ctarget2 = target:cuda()
-      local cmod2 = nn.DistKLDivCriterion(sizeAverage == 1):cuda()
-      a:reset()
-      local cout2 = cinput2.nn.DistKLDivCriterion_updateOutput2(cmod,cinput2,ctarget2)
-      local cgin2 = cinput2.nn.DistKLDivCriterion_updateGradInput2(cmod,cinput2,ctarget2)
-      cutorch.synchronize()
-      tm2.gpu = a:time().real
-
       mytester:assertlt(math.abs(fout-cout), precision_forward, 'error  on output')
       local gerr = cgin:float() - fgin
-      mytester:assertlt(gerr:abs():max(), precision_forward, 'error  on gradInput')
-
-      mytester:assertlt(math.abs(fout-cout2), precision_forward, 'error  on output - 2')
-      local gerr2 = cgin2:float() - fgin
-      mytester:assertlt(gerr2:abs():max(), precision_forward, 'error  on gradInput -2')
+      mytester:assertlt(gerr:abs():max(), precision_backward, 'error  on gradInput')
    end
 end
 
