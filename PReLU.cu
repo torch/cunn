b2d09b6a35ca5dd97d1a75a769840ad8241633d3
adampolyak <Adampolyak@gmail.com>
2015-04-12 18:17:23 +0300

    forward works

--- PReLU.cu
+++ PReLU.cu
@@ -2,13 +2,38 @@
 #include "utils.h"
 
 
+struct PReLUUpdateOutput {
+  const float weight_;
+
+  PReLUUpdateOutput(float weight): weight_(weight) {}
+
+  __device__ __forceinline__ void operator()(float* out, float* in) {
+    float x = *in;
+    *out = (x > 0) ? x : weight_ * x;
+  }
+};
+
 static int cunn_PReLU_updateOutput(lua_State *L)
 {
   THCState *state = getCutorchState(L);
   // Input:
-  THCudaTensor *input = (THCudaTensor*)luaT_checkudata(L, 2, "torch.CudaTensor");
+  THCudaTensor *input = (THCudaTensor*) luaT_checkudata(L, 2, "torch.CudaTensor");
+  // Params:
+  THCudaTensor *weight = (THCudaTensor*) luaT_getfieldcheckudata(L, 1, "weight", "torch.CudaTensor");
+  THCudaTensor *output = (THCudaTensor*) luaT_getfieldcheckudata(L, 1, "output", "torch.CudaTensor");
+  long nOutputPlane = luaT_getfieldchecknumber(L, 1, "nOutputPlane");
 
+  THCudaTensor_resizeAs(state, output, input);
 
+  if (nOutputPlane == 0)
+  {
+    // handle shared parameter case
+    float w = THCudaTensor_get1d(state, weight, 0);
+    THCudaTensor_pointwiseApply2(state, output, input, PReLUUpdateOutput(w));
+  } else {
+    printf("PReLU cuda version only supports shared parameter case\n");
+    THError("aborting");
+  }
 
   THCudaCheck(cudaGetLastError());
   return 1;
