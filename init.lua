880ce65c268cd9240737b707eca0d2a6b69371f5
Adam Lerer <alerer@fb.com>
2015-05-11 16:02:37 -0700

    Revert "Add :cudaOn() and TransferGPU module."
    This reverts commit f1d93d4f5cb2e93bf80cfa507d05f575065c9eae.

--- init.lua
+++ init.lua
@@ -3,15 +3,5 @@ require "nn"
 require "libcunn"
 
 include('test.lua')
-include('utils.lua')
 
 include('DataParallelTable.lua')
-include('TransferGPU.lua')
-
-function nn.Module:cudaOn(device)
-  return nn.utils.recursiveCudaOn(self, device)
-end
-
-function nn.Criterion:cudaOn(device)
-  return nn.utils.recursiveCudaOn(self, device)
-end
