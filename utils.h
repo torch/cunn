7a30e658cbc821abf527636e1ae4a77f085e9929
Andreas Köpf <andreas.koepf@xamla.com>
2016-01-24 00:47:28 +0100

    Move {MarginCriterion, MSECriterion, MultiMarginCriterion, PReLU}.cu -> lib/THCUNN

--- utils.h
+++ utils.h
@@ -26,12 +26,9 @@ void cunn_SpatialFractionalMaxPooling_init(lua_State *L);
 void cunn_SpatialAdaptiveMaxPooling_init(lua_State *L);
 void cunn_SpatialSubSampling_init(lua_State *L);
 void cunn_SpatialAveragePooling_init(lua_State *L);
-void cunn_MultiMarginCriterion_init(lua_State *L);
-void cunn_MarginCriterion_init(lua_State *L);
 void cunn_Square_init(lua_State *L);
 void cunn_Sqrt_init(lua_State *L);
 void cunn_Threshold_init(lua_State *L);
-void cunn_MSECriterion_init(lua_State *L);
 void cunn_SmoothL1Criterion_init(lua_State *L);
 void cunn_SoftPlus_init(lua_State *L);
 void cunn_SoftShrink_init(lua_State *L);
@@ -40,7 +37,6 @@ void cunn_VolumetricConvolution_init(lua_State *L);
 void cunn_VolumetricFullConvolution_init(lua_State *L);
 void cunn_VolumetricMaxPooling_init(lua_State *L);
 void cunn_VolumetricAveragePooling_init(lua_State *L);
-void cunn_PReLU_init(lua_State *L);
 void cunn_RReLU_init(lua_State *L);
 
 #endif
