c6f67e160d55a1dfa27f8c32e5d8605fe012c8f9
Gregory Chanan <gchanan@fb.com>
2016-10-06 15:40:02 -0700

    Add generic support for PReLU.
    This is the first instance of functions that take a lua number but
    are not reals in C.  So, instead of automatically converting lua
    numbers in the half case, we parse the function definitions to
    find the argument positions to convert.

--- THCUNN.lua
+++ THCUNN.lua
@@ -107,13 +107,51 @@ local function extract_function_names_generic(s)
    return t
 end
 
+local function find_positions(s, p)
+   local begin = 0
+   local positions = {}
+   while true do
+      local start, stop = string.find(s, p, begin)
+      if (start == nil) then break end
+      positions[#positions+1] = start
+      begin = stop + 1
+   end
+   return positions
+end
+
+local function extract_function_names_and_real_args(s)
+   local t = {}
+   for n in string.gmatch(s, 'TH_API ([^;]+)') do
+      local func_name = string.match(n, 'void THNN_%(([%a%d_]+)%)')
+      local param_positions = find_positions(n, ',')
+      local positions = {}
+      for x,y in ipairs(find_positions(n, 'real')) do
+          local found = false
+          for cn,cp in ipairs(param_positions) do
+              if cp > y then
+                positions[#positions+1] = cn
+                found = true
+                break
+              end
+          end
+          -- it is the last param
+          if not found then positions[#positions+1] = #param_positions + 1 end
+      end
+
+   t[func_name] = positions
+   end
+   return t
+end
+
+local real_args = extract_function_names_and_real_args(THCUNN_generic_h)
+
 -- build function table
 local function_names = extract_function_names(THCUNN_h)
 local function_names_generic = extract_function_names_generic(THCUNN_generic_h)
 
 -- combine function names for CudaTensor
-for k,v in pairs(function_names_generic) do
-  function_names[#function_names+1] = v
+for k,v in pairs(real_args) do
+  function_names[#function_names+1] = k
 end
 
 THNN.kernels['torch.CudaTensor'] = THNN.bind(THCUNN.C, function_names, 'Cuda', THCUNN.getState)
@@ -122,20 +160,20 @@ torch.getmetatable('torch.CudaTensor').THNN = THNN.kernels['torch.CudaTensor']
 THNN.kernels['torch.CudaDoubleTensor'] = THNN.bind(THCUNN.C, function_names_generic, 'CudaDouble', THCUNN.getState)
 torch.getmetatable('torch.CudaDoubleTensor').THNN = THNN.kernels['torch.CudaDoubleTensor']
 
--- in order to call 'half' functions from lua, convert number arguments from
--- float to half
-local transform_args_to_half = function(t)
-    for k,v in pairs(t) do
-        if torch.type(v) == 'number' then
-          t[k] = ffi.C.THC_float2half(t[k])
-        end
+-- in order to call 'half' functions from lua, convert real arguments from
+-- to half since there is no other defined conversion
+local transform_reals_to_half = function(t, func_name, real_args)
+    for k,v in ipairs(real_args[func_name]) do
+        -- first argument (THCState) is added implicitly by bind
+        t[v-1] = ffi.C.THC_float2half(t[v-1])
     end
     return t
 end
 
 local raw_half_functions = THNN.bind(THCUNN.C, function_names_generic, 'CudaHalf', THCUNN.getState)
 for k,v in pairs(raw_half_functions) do
-    raw_half_functions[k] = function(...) v(unpack(transform_args_to_half({...}))) end
+    raw_half_functions[k] = function(...) v(unpack(transform_reals_to_half({...}, k, real_args)))
+end
 end
 THNN.kernels['torch.CudaHalfTensor'] = raw_half_functions
 torch.getmetatable('torch.CudaHalfTensor').THNN = THNN.kernels['torch.CudaHalfTensor']
