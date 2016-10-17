c138aca5ecbe533fd9783e3051d259d90b3c56e5
Gregory Chanan <gchanan@fb.com>
2016-10-17 11:48:10 -0700

    Add generic support for SpatialUpSamplingBilinear.
    Math is done at accreal precision.  At real precision,
    forward pass fails, but backward passes.  We do backward
    pass at accreal precision for consistency.

--- test.lua
+++ test.lua
@@ -4094,34 +4094,22 @@ function cunntest.SpatialUpSamplingBilinear_forward()
    local w = torch.random(3, 15)
    local scale = torch.random(2,5)
 
-   local tm = {}
-   local title =
-   string.format('SpatialUpSamplingBilinear.forward %dx%dx%d -> %dx%dx%d',
-                               f, h, w, f, h*scale, w*scale)
-   times[title] = tm
+   for k, typename in ipairs(typenames) do
+      local input = torch.randn(f, h, w):type(typename)
 
-   local input = torch.randn(f, h, w)
-   local sconv = nn.SpatialUpSamplingBilinear(scale)
-   local groundtruth = sconv:forward(input)
-   local a = torch.Timer()
-   for i = 1,nloop do
-      groundtruth = sconv:forward(input)
+      local ctype = t2cpu[typename]
+      input = input:type(ctype)
+      local sconv = nn.SpatialUpSamplingBilinear(scale):type(ctype)
+      local groundtruth = sconv:forward(input)
+
+      input = input:type(typename)
+      local gconv = sconv:clone():type(typename)
+      local rescuda = gconv:forward(input)
+
+      local error = rescuda:double() - groundtruth:double()
+      mytester:assertlt(error:abs():max(), precision_forward_type(precision_forward, typename),
+                        string.format('error on state (forward) with %s', typename))
    end
-   tm.cpu = a:time().real
-
-   input = input:cuda()
-   local gconv = sconv:clone():cuda()
-   local rescuda = gconv:forward(input)
-   a:reset()
-   for i = 1,nloop do
-      rescuda = gconv:forward(input)
-   end
-   cutorch.synchronize()
-   tm.gpu = a:time().real
-
-   local error = rescuda:float() - groundtruth
-   mytester:assertlt(error:abs():max(), precision_forward,
-                      'error on state (forward) ')
 end
 
 function cunntest.SpatialUpSamplingBilinear_forward_batch()
@@ -4131,34 +4119,22 @@ function cunntest.SpatialUpSamplingBilinear_forward_batch()
    local w = torch.random(3, 15)
    local scale = torch.random(2,5)
 
-   local tm = {}
-   local title =
-   string.format('SpatialUpSamplingBilinear.forward %dx%dx%dx%d -> %dx%dx%dx%d',
-                               nbatch, f, h, w, nbatch, f, h*scale, w*scale)
-   times[title] = tm
+   for k, typename in ipairs(typenames) do
+      local input = torch.randn(nbatch, f, h, w):type(typename)
 
-   local input = torch.randn(nbatch, f, h, w)
-   local sconv = nn.SpatialUpSamplingBilinear(scale)
-   local groundtruth = sconv:forward(input)
-   local a = torch.Timer()
-   for i = 1,nloop do
-      groundtruth = sconv:forward(input)
+      local ctype = t2cpu[typename]
+      input = input:type(ctype)
+      local sconv = nn.SpatialUpSamplingBilinear(scale):type(ctype)
+      local groundtruth = sconv:forward(input)
+
+      input = input:type(typename)
+      local gconv = sconv:clone():type(typename)
+      local rescuda = gconv:forward(input)
+
+      local error = rescuda:double() - groundtruth:double()
+      mytester:assertlt(error:abs():max(), precision_forward_type(precision_forward, typename),
+                        string.format('error on state (forward) with %s', typename))
    end
-   tm.cpu = a:time().real
-
-   input = input:cuda()
-   local gconv = sconv:clone():cuda()
-   local rescuda = gconv:forward(input)
-   a:reset()
-   for i = 1,nloop do
-      rescuda = gconv:forward(input)
-   end
-   cutorch.synchronize()
-   tm.gpu = a:time().real
-
-   local error = rescuda:float() - groundtruth
-   mytester:assertlt(error:abs():max(), precision_forward,
-                      'error on state (forward) ')
 end
 
 function cunntest.SpatialUpSamplingBilinear_backward()
@@ -4167,43 +4143,30 @@ function cunntest.SpatialUpSamplingBilinear_backward()
    local w = torch.random(3, 15)
    local scale = torch.random(2,5)
 
-   local tm = {}
-   local title =
-   string.format('SpatialUpSamplingBilinear.backward %dx%dx%d -> %dx%dx%d',
-                               f, h, w, f, h*scale, w*scale)
-   times[title] = tm
+   for k, typename in ipairs(typenames) do
+      local input = torch.randn(f, h, w):type(typename)
+      local gradOutput = torch.randn(f, h*scale, w*scale):type(typename)
 
-   local input = torch.randn(f, h, w)
-   local gradOutput = torch.randn(f, h*scale, w*scale)
-   local sconv = nn.SpatialUpSamplingBilinear(scale)
-   sconv:forward(input)
-   sconv:zeroGradParameters()
-   local groundgrad = sconv:backward(input, gradOutput)
-   local a = torch.Timer()
-   for i = 1,nloop do
+      local ctype = t2cpu[typename]
+      input = input:type(ctype)
+      gradOutput = gradOutput:type(ctype)
+      local sconv = nn.SpatialUpSamplingBilinear(scale):type(ctype)
+      sconv:forward(input)
       sconv:zeroGradParameters()
-      groundgrad = sconv:backward(input, gradOutput)
-   end
-   tm.cpu = a:time().real
+      local groundgrad = sconv:backward(input, gradOutput)
 
-   input = input:cuda()
-   gradOutput = gradOutput:cuda()
-   local gconv = sconv:clone():cuda()
-   gconv:forward(input)
-   gconv:zeroGradParameters()
-   local rescuda = gconv:backward(input, gradOutput)
-   a:reset()
-   for i = 1,nloop do
+      input = input:type(typename)
+      gradOutput = gradOutput:type(typename)
+      local gconv = sconv:clone():type(typename)
+      gconv:forward(input)
       gconv:zeroGradParameters()
-      rescuda = gconv:backward(input, gradOutput)
+      local rescuda = gconv:backward(input, gradOutput)
+
+      local error = rescuda:double() - groundgrad:double()
+
+      mytester:assertlt(error:abs():max(), precision_backward_type(precision_backward, typename),
+                        string.format('error on state (backward) with %s', typename))
    end
-   cutorch.synchronize()
-   tm.gpu = a:time().real
-
-   local error = rescuda:float() - groundgrad
-
-   mytester:assertlt(error:abs():max(), precision_backward,
-                      'error on state (backward) ')
 end
 
 function cunntest.SpatialUpSamplingBilinear_backward_batch()
@@ -4213,24 +4176,30 @@ function cunntest.SpatialUpSamplingBilinear_backward_batch()
    local w = torch.random(3, 15)
    local scale = torch.random(2,5)
 
-   local input = torch.randn(nbatch, f, h, w)
-   local gradOutput = torch.randn(nbatch, f, h*scale, w*scale)
-   local sconv = nn.SpatialUpSamplingBilinear(scale)
-   local output = sconv:forward(input)
-   sconv:zeroGradParameters()
-   local groundgrad = sconv:backward(input, gradOutput)
+   for k, typename in ipairs(typenames) do
+      local input = torch.randn(nbatch, f, h, w):type(typename)
+      local gradOutput = torch.randn(nbatch, f, h*scale, w*scale):type(typename)
 
-   input = input:cuda()
-   gradOutput = gradOutput:cuda()
-   local gconv = sconv:clone():cuda()
-   gconv:forward(input)
-   gconv:zeroGradParameters()
-   local rescuda = gconv:backward(input, gradOutput)
+      local ctype = t2cpu[typename]
+      input = input:type(ctype)
+      gradOutput = gradOutput:type(ctype)
+      local sconv = nn.SpatialUpSamplingBilinear(scale):type(ctype)
+      local output = sconv:forward(input)
+      sconv:zeroGradParameters()
+      local groundgrad = sconv:backward(input, gradOutput)
 
-   local err = rescuda:float() - groundgrad
+      input = input:type(typename)
+      gradOutput = gradOutput:type(typename)
+      local gconv = sconv:clone():type(typename)
+      gconv:forward(input)
+      gconv:zeroGradParameters()
+      local rescuda = gconv:backward(input, gradOutput)
 
-   mytester:assertlt(err:abs():max(), precision_backward,
-                      'error on state (backward) ')
+      local err = rescuda:double() - groundgrad:double()
+
+      mytester:assertlt(err:abs():max(), precision_backward_type(precision_backward, typename),
+                        string.format('error on state (backward) with %s', typename))
+   end
 end
 
 function cunntest.l1cost()
