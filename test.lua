125992ee49521cf6e5ddfe0d160317d24c73ddc6
Gregory Chanan <gchanan@fb.com>
2016-10-25 13:08:50 -0700

    Add generic support for L1SmoothCriterion.

--- test.lua
+++ test.lua
@@ -3422,31 +3422,33 @@ end
 function cunntest.SmoothL1()
    for sizeAverage = 0, 1 do
       local size = math.random(3000,5000)
-      local input = torch.randn(size,1,1)
-      local target = torch.randn(size)
-      local mod = nn.SmoothL1Criterion(sizeAverage == 1)
 
-      local tm = {}
-      local title = string.format('SmoothL1Criterion sizeAverage %d, %d ', sizeAverage, size)
-      times[title] = tm
+      for k, typename in ipairs(typenames) do
+         local input = torch.randn(size,1,1):type(typename)
+         local target = torch.randn(size):type(typename)
 
-      local a = torch.Timer()
-      local fout = mod:forward(input,target)
-      local fgin = mod:backward(input,target):clone()
-      tm.cpu = a:time().real
+         local ctype = t2cpu[typename]
+         input = input:type(ctype)
+         target = target:type(ctype)
+         local mod = nn.SmoothL1Criterion(sizeAverage == 1):type(ctype)
 
-      local cinput = input:cuda()
-      local ctarget = target:cuda()
-      local cmod = nn.SmoothL1Criterion(sizeAverage == 1):cuda()
-      a:reset()
-      local cout = cmod:forward(cinput,ctarget)
-      local cgin = cmod:backward(cinput,ctarget)
-      cutorch.synchronize()
-      tm.gpu = a:time().real
+         local fout = mod:forward(input,target)
+         local fgin = mod:backward(input,target):clone()
 
-      mytester:assertlt(math.abs(fout-cout), 0.01, 'error  on output')
-      local gerr = cgin:float() - fgin
-      mytester:assertlt(gerr:abs():max(), precision_forward, 'error  on gradInput')
+         local cinput = input:type(typename)
+         local ctarget = target:type(typename)
+         local cmod = nn.SmoothL1Criterion(sizeAverage == 1):type(typename)
+         local cout = cmod:forward(cinput,ctarget)
+         local cgin = cmod:backward(cinput,ctarget)
+
+         if (typename == 'torch.CudaHalfTensor') then
+            fout = ffi.C.THC_half2float(ffi.C.THC_float2half(fout))
+         end
+         mytester:assertlt(math.abs(fout-cout), 0.01, string.format('error  on output with %s', typename))
+         local gerr = cgin:double() - fgin:double()
+         mytester:assertlt(gerr:abs():max(), precision_forward_type(precision_forward, typename),
+            string.format('error  on gradInput with %s', typename))
+      end
    end
 end
 
